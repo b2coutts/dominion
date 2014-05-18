4c17ae31f7aeb8c0c0193b5b7cfd6fbdd31e6b98
Bryan Coutts <b2coutts@uwaterloo.ca>
2014-05-17 20:04:33 -0400

    Added standard kingdom card sets to Config

--- Config.hs
+++ Config.hs
@@ -9,5 +9,20 @@ userNames :: [String]
 userNames = ["alice", "bob"]
 
 -- list of the kingdom cards being used in the game
-cardList = ["throneroom", "moneylender", "remodel", "councilroom", "spy",
-            "moat", "village", "market", "adventurer", "mine"]
+cardList = villageSquare
+
+-- several standard configurations
+firstGame = words "cellar market militia mine moat remodel smithy village\
+                 \ woodcutter workshop"
+
+bigMoney = words "adventurer bureaucrat chancellor chapel feast laboratory\
+                \ market mine moneylender throneroom"
+
+interaction = words "bureaucrat chancellor councilroom festival library\
+                   \ militia moat spy thief village"
+
+sizeDistortion = words "cellar chapel feast gardens laboratory thief village\
+                      \ witch woodcutter workshop"
+
+villageSquare = words "bureaucrat cellar festival library market remodel\
+                     \ smithy throneroom village woodcutter"
